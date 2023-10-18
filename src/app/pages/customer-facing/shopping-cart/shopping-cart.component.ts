3eee1ba55ad928f0931a062a17a2d8a60ca1338c
Sam <41166645+koketsosamuel@users.noreply.github.com>
2023-10-18 02:51:04 +0200

    Fix shopping cart loaders

--- src/app/pages/customer-facing/shopping-cart/shopping-cart.component.ts
+++ src/app/pages/customer-facing/shopping-cart/shopping-cart.component.ts
@@ -6,34 +6,38 @@ import { LoadingSpinnerService } from 'src/app/services/loading-spinner/loading-
 @Component({
   selector: 'app-shopping-cart',
   templateUrl: './shopping-cart.component.html',
-  styleUrls: ['./shopping-cart.component.scss']
+  styleUrls: ['./shopping-cart.component.scss'],
 })
 export class ShoppingCartComponent implements OnInit {
-
   items: ICartItem[] = [];
   total: number = 0;
   vat: number = 0;
   subTotal: number = 0;
+  loading = true;
 
-  constructor(private readonly cartService: CartService, private readonly loadingSpinnerService: LoadingSpinnerService) {
-
-  }
+  constructor(
+    private readonly cartService: CartService,
+    private readonly loadingSpinnerService: LoadingSpinnerService
+  ) {}
 
   ngOnInit() {
-    this.loadingSpinnerService.show();
-    this.cartService.getUserCart().then(cart => {
-      this.items = cart.cartItems as ICartItem[];
-      this.setOrderSummary()
-    }).finally(() => {
-      this.loadingSpinnerService.hide();
-    })
+    this.loading =true;
+    this.cartService
+      .getUserCart()
+      .then((cart) => {
+        this.items = cart.cartItems as ICartItem[];
+        this.setOrderSummary();
+      })
+      .finally(() => {
+        this.loading = false;
+      });
   }
 
   setOrderSummary() {
     let sum = 0;
-    this.items.forEach(i => sum+=i.totalPrice);
+    this.items.forEach((i) => (sum += i.totalPrice));
     this.total = sum;
-    this.vat = Number((this.total * 0.15).toFixed(2))
+    this.vat = Number((this.total * 0.15).toFixed(2));
     this.subTotal = this.total - this.vat;
   }
 }
